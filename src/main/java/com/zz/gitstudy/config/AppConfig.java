da4f5afb6f14923b1445f41fa8e8c963c0fa2f11
zhangfx <zhangfuxing1010@163.com>
2023-02-15 09:49:11 +0800

    应用配置文件

---
+++ src/main/java/com/zz/gitstudy/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.zz.gitstudy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhangfx
+ * @date 2023/2/15
+ */
+@Configuration
+public class AppConfig {
+}
